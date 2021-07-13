0a193ce0b966da211ef1a1d95355b551983a3f80
qkrthdus605 <qkrthdus605@gmail.com>
2021-07-13 21:41:16 +0900

    first commit

---
+++ src/Hello.js
@@ -0,0 +1,9 @@
+import React from 'react';
+// 리액트를 불러옴
+
+function Hello(){
+  return <div>안녕하세요</div>
+}
+
+// Hello라는 컴포넌트를 내보내겠다는 의미. 다른 컴포넌트에서 불러와 사용할 수 있음
+export default Hello;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// 브라우저에 있는 실제 DOM 내부에 리액트 컴포넌트를 렌더링하겠다는 의미
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
+  // id가 root인 DOM을 선택 
 );
 
 // If you want to start measuring performance in your app, pass a function
