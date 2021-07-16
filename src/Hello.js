062ed556f47661d47a3e90c756f2da189d2e3597
qkrthdus605 <qkrthdus605@gmail.com>
2021-07-16 16:52:18 +0900

    props 사용해보기

--- src/Hello.js
+++ src/Hello.js
@@ -1,9 +1,12 @@
 import React from 'react';
 // 리액트를 불러옴
 
-function Hello(){
-  return <div>안녕하세요</div>
+function Hello({color, name}){
+  return <div style={{ color }}>안녕하세요 {name} </div>
 }
 
+Hello.defaultProps = {
+  name: '이름없음'
+}
 // Hello라는 컴포넌트를 내보내겠다는 의미. 다른 컴포넌트에서 불러와 사용할 수 있음
 export default Hello;
