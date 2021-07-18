2ce7d5e6fcb6e694edd2d9260b55c1fb4d230e80
qkrthdus605 <qkrthdus605@gmail.com>
2021-07-18 13:13:48 +0900

    useState 함수를 사용해 컴포넌트에서 바뀌는 값 관리하기

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import Hello from './Hello';
-import Wrapper from './Wrapper';
+import Counter from './Counter';
 
 function App() {
   return (
-    <Wrapper>
-      <Hello name="react" color="red" isSpecial={true} />
-      {/* '={true}'는 생략 가능, 생략 시 true로 간주 */}
-      <Hello color="pink" />
-    </Wrapper>
+    <Counter />
   );
 }
 
